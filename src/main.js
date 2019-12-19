cb4aa34aa05c610c1b22f0682aeab74b1a165ea3
litchi0509 <litchi0509@gmail.com>
2019-12-19 16:46:44 +0800

    ...

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/en';
+import VueRouter from 'vue-router';
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI, { size: 'small', zIndex: 2000, locale, VueRouter });
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
